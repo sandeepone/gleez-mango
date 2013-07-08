577e511e899ca694389640de5b9b4069b66e4e16
Sergey Yakovlev <sadhooklay@gmail.com>
2013-07-08 18:26:42 +0400

    New feature: Added Russian lang

---
+++ i18n/ru.php
@@ -0,0 +1,67 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Localized strings into Russian
+ *
+ * @package   Gleez\Mango\I18n
+ * @author    Sergey Yakovlev - Gleez
+ * @copyright (c) 2011-2013 Gleez Technologies
+ * @license   http://gleezcms.org/license Gleez CMS License
+ */
+return array
+(
+	// !
+	// :
+	// @
+	// %
+	// A
+	'All logs' => 'Все сообщения',
+	'An error occurred when clearing the system log: %msg' => 'Произошла ошибка при очистке системного журнала: %msg',
+	'An error occurred when deleting the message: %msg' => 'Произошла ошибка при удалении сообщения: %msg',
+	// B
+	// C
+	'Class' => 'Класс',
+	'Clear all' => 'Очистить всё',
+	'Clear all messages from the log' => 'Очистить все сообщения из журнала',
+	// D
+	'Delete :id' => 'Удалить :id',
+	'Delete this entry from log' => 'Удалить запись из журнала',
+	'Drop system log' => 'Очистить системный журнал',
+	// E
+	// F
+	'Field' => 'Поле',
+	'File' => 'Файл',
+	'Function' => 'Функция',
+	// G
+	// H
+	'Here shows the complete information about current entry from the System log.' => 'Здесь представлена ​​полная информация о текущей записи из системного журнала.',
+	// I
+	// J
+	// K
+	// L
+	'Line' => 'Строка',
+	'Log #:id' => 'Сообщение #:id',
+	// M
+	'Message' => 'Сообщение',
+	'Message #%id not found!' => 'Сообщение #%id не найдено!',
+	'Message from the log has been removed' => 'Сообщение из системного журнала успешно удалено',
+	// N
+	// O
+	// P
+	// Q
+	// R
+	'Refer' => 'Перенаправлено с',
+	// S
+	'System log' => 'Системный журнал',
+	'System log successfully cleared. Database message: %msg' => 'Системный журнал успешно очищен. Ответ базы данных: %msg',
+	// T
+	'The log component monitors your website, capturing system events in a log to be reviewed by an authorized individual at a later time. The log is simply a list of recorded events containing usage data, performance data, errors, warnings and operational information. It is vital to check the log report on a regular basis as it is often the only way to tell what is going on.' => 'Компонент журналирования занимается мониторингом вашего сайта, захватывая все системные события в журнал и предоставляя возможность просмотра событий уполномоченным лицом в более позднее время. Системный журнал является списком записанных событий, содержащих данные об использовании вашего сайта, данные о производительности, ошибки, предупреждения и прочую оперативную информацию. Крайне важно проверять его на регулярной основе, так как часто это единственный способ узнать, что происходит.',
+	// U
+	'User Agent' => 'Клиентское приложение',
+	// V
+	'Value' => 'Значение',
+	'View log' => 'Просмотр сообщения',
+	// W
+	// X
+	// Y
+	// Z
+);
