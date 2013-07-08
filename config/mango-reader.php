8e193ca0efa153f5960fb65b62a172557cdc0603
Sergey Yakovlev <sadhooklay@gmail.com>
2013-07-08 18:24:33 +0400

    Fixed issue: Removed old configs

--- config/mango-reader.php
+++ config/mango-reader.php
@@ -1,36 +1,9 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return array(
+	'collections' => array(
+		'logs' => 'Logs'
+	),
 
-  /**
-   * Configuration Name
-   *
-   * You use this name when initializing a new MongoDB instance
-   *
-   *  // Example:
-   *  $db = MongoDB::instance('default');
-   */
-  'default' => array(
-
-    /** @var array Connection Setup */
-    'connection' => array(
-
-      /** @var string Database host */
-      //'hostname' => '192.168.0.1',
-
-      /** @var string Database name */
-      //'database'  => 'Cerber',
-
-      /** @var string Auth params */
-      //'username'  => 'username',
-      //'password'  => 'password',
-
-      /** @var array Additional options */
-      //'options'   => array(
-      //  'persist'    => 'persist_id',
-      //  'timeout'    => 1000,
-      //  'replicaSet' => TRUE
-      //)
-    ),
-  )
+	'items_per_page' => 30,
 );
