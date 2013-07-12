e0ab6b2ec24bc81484a6e489bf1a3936805b0b71
Sergey Yakovlev <sadhooklay@gmail.com>
2013-07-12 08:32:14 +0400

    New feature: Added View for Statistics

---
+++ views/admin/log/stat.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') OR die('No direct script access allowed.');
+/**
+ * Created by JetBrains PhpStorm.
+ * User: klay
+ * Date: 12.07.13
+ * Time: 7:20
+ * To change this template use File | Settings | File Templates.
+ */
