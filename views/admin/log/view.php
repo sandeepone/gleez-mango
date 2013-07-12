3620dd742257bcf6cdb337658564e0d5b1578e75
Sergey Yakovlev <sadhooklay@gmail.com>
2013-07-12 08:36:57 +0400

    Fixed issue: Fixed indentation

--- views/admin/log/view.php
+++ views/admin/log/view.php
@@ -16,59 +16,59 @@
 	</tr>
 	</thead>
 	<tbody>
-	<tr>
-		<td><?php _e('ID'); ?></td>
-		<td><?php echo $log['_id']; ?></td>
-	</tr>
-	<tr>
-		<td><?php _e('Type')?></td>
-		<td>
-			<?php echo HTML::label($log['level'], $log['level']); ?>
-		</td>
-	</tr>
-	<tr>
-		<td><?php _e('Date')?></td>
-		<td>
-			<?php echo Date::formatted_time($log['time']->sec, Config::get('site.date_time_format', 'l, F j, Y - H:i'), Config::get('site.timezone', 'UTC')); ?>
-		</td>
-	</tr>
-	<tr>
-		<td><?php _e('Host')?></td>
-		<td><?php echo $log['hostname']; ?></td>
-	</tr>
-	<tr>
-		<td><?php _e('User Agent')?></td>
-		<td><?php echo ($log['user_agent']) ? $log['user_agent'] : '&mdash;' ?></td>
-	</tr>
-	<tr>
-		<td><?php _e('File')?></td>
-		<td><?php echo Text::plain($log['file']) ?></td>
-	</tr>
-	<tr>
-		<td><?php _e('Line')?></td>
-		<td><?php echo Text::plain($log['line']) ?></td>
-	</tr>
-	<tr>
-		<td><?php _e('Class')?></td>
-		<td><?php echo Text::plain($log['class']) ?></td>
-	</tr>
-	<tr>
-		<td><?php _e('Function')?></td>
-		<td><?php echo Text::plain($log['function']) ?></td>
-	</tr>
-	<tr>
-		<td><?php _e('URL')?></td>
-		<td><?php echo Text::plain($log['url']) ?></td>
-	</tr>
-	<?php if (isset($log['refer'])): ?>
 		<tr>
-			<td><?php _e('Refer')?></td>
-			<td><?php echo Text::plain($log['refer']) ?></td>
+			<td><?php _e('ID'); ?></td>
+			<td><?php echo $log['_id']; ?></td>
 		</tr>
-	<?php endif; ?>
-	<tr>
-		<td><?php _e('Message')?></td>
-		<td><?php echo Text::plain($log['body']) ?></td>
-	</tr>
+		<tr>
+			<td><?php _e('Type')?></td>
+			<td>
+				<?php echo HTML::label($log['level'], $log['level']); ?>
+			</td>
+		</tr>
+		<tr>
+			<td><?php _e('Date')?></td>
+			<td>
+				<?php echo Date::formatted_time($log['time']->sec, Config::get('site.date_time_format', 'l, F j, Y - H:i'), Config::get('site.timezone', 'UTC')); ?>
+			</td>
+		</tr>
+		<tr>
+			<td><?php _e('Host')?></td>
+			<td><?php echo $log['hostname']; ?></td>
+		</tr>
+		<tr>
+			<td><?php _e('User Agent')?></td>
+			<td><?php echo ($log['user_agent']) ? $log['user_agent'] : '&mdash;' ?></td>
+		</tr>
+		<tr>
+			<td><?php _e('File')?></td>
+			<td><?php echo Text::plain($log['file']) ?></td>
+		</tr>
+		<tr>
+			<td><?php _e('Line')?></td>
+			<td><?php echo Text::plain($log['line']) ?></td>
+		</tr>
+		<tr>
+			<td><?php _e('Class')?></td>
+			<td><?php echo Text::plain($log['class']) ?></td>
+		</tr>
+		<tr>
+			<td><?php _e('Function')?></td>
+			<td><?php echo Text::plain($log['function']) ?></td>
+		</tr>
+		<tr>
+			<td><?php _e('URL')?></td>
+			<td><?php echo Text::plain($log['url']) ?></td>
+		</tr>
+		<?php if (isset($log['refer'])): ?>
+			<tr>
+				<td><?php _e('Refer')?></td>
+				<td><?php echo Text::plain($log['refer']) ?></td>
+			</tr>
+		<?php endif; ?>
+		<tr>
+			<td><?php _e('Message')?></td>
+			<td><?php echo Text::plain($log['body']) ?></td>
+		</tr>
 	</tbody>
 </table>
