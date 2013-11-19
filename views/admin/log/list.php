4412a8af0c0b2ecd005d18d42bcba87b5c029a1c
Sergey Yakovlev <sadhooklay@gmail.com>
2013-11-19 18:32:10 +0400

    Fixed issue: fix icon sytax from icon to fa

--- views/admin/log/list.php
+++ views/admin/log/list.php
@@ -2,7 +2,7 @@
 	<?php _e('The log is simply a list of recorded events containing usage data, performance data, errors, warnings and operational information. It is vital to check the log report on a regular basis as it is often the only way to tell what is going on.'); ?>
 </div>
 
-<?php echo HTML::anchor($clear_url, '<i class="icon-trash"></i> '.__('Clear all'), array('class' => 'btn btn-danger pull-right', 'title' => __('Clear all'))); ?>
+<?php echo HTML::anchor($clear_url, '<i class="fa fa-trash-o"></i> '.__('Clear all'), array('class' => 'btn btn-danger pull-right', 'title' => __('Clear all'))); ?>
 <div class="clearfix"></div><br>
 
 <table id="log-admin-list" class="table table-striped table-bordered table-highlight">
@@ -24,7 +24,7 @@
 			<td><?php echo HTML::anchor(Route::get('admin/log')->uri(array('action' => 'view', 'id' => $log['_id'])), Text::plain(Text::limit_chars($log['body'], 50)), array('title'=> __('View Log'))); ?></td>
 			<td><?php echo $log['hostname']; ?></td>
 			<td><?php echo Text::limit_chars($log['url'], 30); ?></td>
-			<td><?php echo HTML::icon(Route::get('admin/log')->uri(array('action' =>'delete', 'id' => $log['_id'])), 'icon-trash', array('class'=>'action-delete', 'title'=> __('Delete Log'))) ?></td>
+			<td><?php echo HTML::icon(Route::get('admin/log')->uri(array('action' =>'delete', 'id' => $log['_id'])), 'fa-trash-o', array('class'=>'action-delete', 'title'=> __('Delete Log'))) ?></td>
 		</tr>
 	<?php endforeach; ?>
 	</tbody>
